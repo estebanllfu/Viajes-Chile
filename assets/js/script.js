ae6f5eea3548638c386f36bd5dc6943490988421
estebanllfu <estebanllfu@gmail.com>
2024-07-02 00:23:59 -0400

    Correcciones + integrando Jquery

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+// Codigo Jquery
+$(document).ready(function() {
+  
+//Activando el tooltip de enviar en section contactame
+    $('#tooltip-bs').tooltip();
+
+//alerta cuando se envie datos de contacto
+    $('#tooltip-bs').click(function(){
+
+        alert('Estamos agradecidos de que quieras conectar con nosotros <3');
+        
+    });
+
+//smooth scroll
+var scrollLink = $('.scroll');
+
+scrollLink.click(function(e){
+e.preventDefault();
+$('body, html').animate({
+    scrollTop: $(this.hash).offset().top
+
+},1000 )
+
+})
+
+});
